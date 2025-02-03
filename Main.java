6d7ccf91a9b2f7e8dec95b38a0be2915d5da1b0c
EC2 Default User <ec2-user@ip-172-31-0-87.ap-northeast-1.compute.internal>
2025-02-03 09:08:17 +0000

    add-name

--- Main.java
+++ Main.java
@@ -2,30 +2,33 @@ import java.util.Scanner;
 import java.util.Random;
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         int userHand;
         int computerHand;
+        String name;
+        System.out.println("あなたの名前を教えて下さい：");
+        name = scan.next();
+        if(name.isEmpty()) {
+            name = "ゲスト";
+        };
+        
+        System.out.println("こんにちは" + name + "さん");
         System.out.println("次のうちから一つ選択して数字を入力してください(１.グー、２.チョキ、３.パー)：");
-        Scanner scan = new Scanner(System.in);
           userHand = scan.nextInt();
           Random random = new Random();
         switch (userHand) {
             case 1:
-                System.out.println("あなたが出したのはグーです。");
+                System.out.println(name + "さんが出したのはグーです。");
                 break;
             case 2:
-                System.out.println("あなたが出したのはチョキです。");
+                System.out.println(name +"さんが出したのはチョキです。");
                 break;
             case 3:
-                System.out.println("あなたが出したのはパーです。");
+                System.out.println(name +"さんが出したのはパーです。");
                 break;
             default:
                 System.out.println("最初から入力してください。");
         };
-        System.out.println("結果が分かりました！！");
-        System.out.println("じゃんけんの結果を見るにはEnterキーを押してください。");
-        Scanner scanner = new Scanner(System.in);
-        scanner.nextLine();
-        scanner.close();
         computerHand = random.nextInt(3) +1;
         switch (computerHand) {
             case 1:
@@ -45,9 +48,9 @@ public class Main {
          ((userHand == 1 && computerHand == 2) ||
             (userHand == 2 && computerHand == 3) ||
             (userHand == 3 && computerHand == 1) )
-        {System.out.println("あなたの勝ちです！！");
+        {System.out.println(name + "さんの勝ちです！！");
         }else{
-            System.out.println("あなたの負けです…");
+            System.out.println(name + "さんの負けです…");
         }
     }
 }
